c9c44c9522defb8ce1eb4760e6eaa04c66e5e95d
Yosef Sherif <yasherif92@gmail.com>
2025-08-11 06:05:54 +0200

    init commit

---
+++ client/src/app/(pages)/AuthChecker.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
+import Loading from "../components/loading";
+
+const publicRoutes = ["/login", "/login/factor-one"];
+
+export default function AuthChecker({ children }: { children: React.ReactNode }) {
+  const { isLoaded, user } = useUser();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isChecking, setIsChecking] = useState(true);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    setIsChecking(false);
+
+    // إذا كان المستخدم غير مسجل والصفحة الحالية ليست من الصفحات العامة
+    if (!user && !publicRoutes.includes(pathname)) {
+      router.push("/login");
+      return;
+    }
+
+    // إذا كان المستخدم مسجلًا وهو في صفحة تسجيل/دخول
+    if (user && publicRoutes.includes(pathname)) {
+      router.push("/");
+      return;
+    }
+  }, [isLoaded, user, pathname]);
+
+  if (!isLoaded || isChecking) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <Loading />
+      </div>
+    );
+  }
+
+  return <>{children}</>;
+}
